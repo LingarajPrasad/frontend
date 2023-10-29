5ff9c68b6adac8da716f2c952e31b00e63817352
Lingaraj <lingarajprasadsahu2002@gmail.com>
2023-10-29 16:30:50 +0530

    v1.1

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,31 +1,50 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Search2Icon } from '@chakra-ui/icons'
+import { Box, Button, Flex, Input, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 
 const ChatPage = () => {
-  return (
-    <Box position={'absolute'} left={'50%'}
-    transform={'translateX(-50%)'}
-    p={4}
-    w={{
-        lg:'750px',
-        md:'80%',
-        base:'100%'
-    }}>
-        <Flex gap={4} flexDirection={{
-            base:'column',
-            md:'row'
-        }}
-        maxW={{
-            sm:'400px',
-            md:'full'
-        }}
-        mx={'auto'}
-        >
-<Flex flex={30}>Conversation</Flex>
-<Flex flex={70}>MessageContainer</Flex>
-        </Flex>
-    </Box>
-  )
+    return (
+        <Box position={'absolute'} left={'50%'}
+            transform={'translateX(-50%)'}
+            p={4}
+            w={{
+                lg: '750px',
+                md: '80%',
+                base: '100%'
+            }}>
+            <Flex gap={4} flexDirection={{
+                base: 'column',
+                md: 'row'
+            }}
+                maxW={{
+                    sm: '400px',
+                    md: 'full'
+                }}
+                mx={'auto'}
+            >
+                <Flex flex={30} gap={2} flexDirection={'column'}
+                    maxW={{
+                        sm: '250px',
+                        md: 'full'
+                    }} mx={'auto'}>
+                    <Text
+                        fontWeight={700} color={useColorModeValue('gray.600', 'gray.400')}
+                    >
+                        Your Conversations
+                    </Text>
+                    <form>
+                        <Flex alignItems={'center'} gap={2}>
+                            <Input placeholder='search for a user' />
+                            <Button size={'sm'}>
+                                <Search2Icon />
+                            </Button>
+                        </Flex>
+                    </form>
+                </Flex>
+                <Flex flex={70}>MessageContainer</Flex>
+            </Flex>
+        </Box>
+    )
 }
 
 export default ChatPage
