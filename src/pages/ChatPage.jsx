60107e23bd922ba1986a52ef69fb09a474ce7564
Lingaraj <lingarajprasadsahu2002@gmail.com>
2023-10-29 17:03:23 +0530

    v1.1

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,5 +1,5 @@
 import { Search2Icon } from '@chakra-ui/icons'
-import { Box, Button, Flex, Input, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { SkeletonCircle,Skeleton,Box, Button, Flex, Input, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 
 const ChatPage = () => {
@@ -40,6 +40,18 @@ const ChatPage = () => {
                             </Button>
                         </Flex>
                     </form>
+                    {true &&
+						[0, 1, 2].map((_, i) => (
+							<Flex key={i} gap={4} alignItems={"center"} p={"1"} borderRadius={"md"}>
+								<Box>
+									<SkeletonCircle size={"10"} />
+								</Box>
+								<Flex w={"full"} flexDirection={"column"} gap={3}>
+									<Skeleton h={"10px"} w={"80px"} />
+									<Skeleton h={"8px"} w={"90%"} />
+								</Flex>
+							</Flex>
+						))}
                 </Flex>
                 <Flex flex={70}>MessageContainer</Flex>
             </Flex>
