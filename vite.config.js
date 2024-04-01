261bf53b62645430c9c9719e977431472e449fac
Lingaraj Prasad Sahu <lingarajprasadsahu2002@gmail.com>
2024-04-01 19:38:08 +0530

    Signed-off-by: Lingaraj Prasad Sahu <lingarajprasadsahu2002@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,8 @@ export default defineConfig({
     //get rid of CORS error
     proxy:{
       "/api":{
-        // target:'http://127.0.0.1:3001',
-        target:'http://13.200.79.249:3001',
+        target:'http://127.0.0.1:3001',
+        // target:'http://13.200.79.249:3001',
         // changeOrigin:true,
         // secure:true
       }
